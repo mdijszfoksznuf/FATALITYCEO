5a0ba5f2c202e524f8729b8e0a0d77f08815fd81
mdijszfoksznuf <fernam446@gmail.com>
2026-04-29 20:26:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.faq-q').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const item = btn.parentElement;
+    const isOpen = item.classList.contains('open');
+    document.querySelectorAll('.faq-item').forEach(i => i.classList.remove('open'));
+    if (!isOpen) item.classList.add('open');
+  });
+});
